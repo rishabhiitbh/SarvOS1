92a7c1582ed99164cf894a998373c2782981ef5c
Rishabh Poddar <rishabh.poddar0207@gmail.com>
2018-01-21 00:28:04 +0530

    printstring

--- boot_asm.s
+++ boot_asm.s
@@ -1,35 +1,9 @@
 [org 0x7c00]            ;loading address of bootloader
-mov dx,0x567d
-call printhex
+mov si,my_message
+call printstring
 
 jmp $                   ;hang the cpu
 %include "include/asm/print.s"
-printhex:
-
-    mov ax,dx
-    shr ax,12
-    mov ax,hexcode + al
-    mov [hextemplate+2],al
-
-    mov ax,dx
-    shr ax,8
-    mov [hextemplate+3],al    
-    
-    mov ax,dx
-    shr ax,4
-    mov [hextemplate+4],al    
-    
-    mov ax,dx
-    mov [hextemplate+5],al 
-
-    mov si ,hextemplate 
-    call printstring
-
-ret
-hextemplate:
-    db'0x????',0
-hexcode:
-    db '0123456789abcdef' 
 my_message:
     db 'hello printing from the bootloader',0
 
